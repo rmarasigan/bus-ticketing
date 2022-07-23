07fadf2dd54553efb2c940955b630f82027ae6cc
rmarasigan <russianhielle@gmail.com>
2022-07-23 11:22:59 +0800

    ADD: StatusOK, StatusUnhandledMethod and StatusUnhandledRequest response

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -10,13 +11,24 @@ type Error struct {
 	Message string `json:"error_msg,omitempty"`
 }
 
-// Response returns a response to be returned by the API Gateway Reequest.
+// Response returns a response to be returned by the API Gateway Request.
 func Response(status int, body interface{}) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{
 		Headers: map[string]string{
 			"Content-Type": "application/json",
 		},
 		StatusCode: status,
+		Body:       string(EncodeResponse(body)),
+	}, nil
+}
+
+// StatusOK returns a response of an HTTP StatusOK with body.
+func StatusOK(body interface{}) (*events.APIGatewayProxyResponse, error) {
+	return &events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		StatusCode: http.StatusOK,
 		Body:       string(EncodeResponse(body)),
 	}, nil
 }
@@ -31,5 +43,31 @@ func StatusBadRequest(err error) (*events.APIGatewayProxyResponse, error) {
 		},
 		StatusCode: http.StatusBadRequest,
 		Body:       string(body),
+	}, nil
+}
+
+// StatusUnhandledMethod returns a response of an HTTP StatusMethodNotAllowed and an error message of unhandled method.
+func StatusUnhandledMethod() (*events.APIGatewayProxyResponse, error) {
+	body := string(EncodeResponse(Error{Message: errors.New("unhandled method").Error()}))
+
+	return &events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		StatusCode: http.StatusMethodNotAllowed,
+		Body:       body,
+	}, nil
+}
+
+// StatusUnhandledRequest returns a response of an HTTP StatusNotImplemented and an error message.
+func StatusUnhandledRequest(err error) (*events.APIGatewayProxyResponse, error) {
+	body := string(EncodeResponse(Error{Message: err.Error()}))
+
+	return &events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		StatusCode: http.StatusNotImplemented,
+		Body:       body,
 	}, nil
 }

9f25faeca0bcea4a845ffb7204a50e515d0fdfd8
rmarasigan <russianhielle@gmail.com>
2022-07-26 09:05:33 +0800

    ADD: DynamoDBAttributesResponse

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -6,14 +6,26 @@ import (
 	cw "github.com/rmarasigan/bus-ticketing/pkg/cw/logger"
 )
 
-// DynamoDBAttributeResponse returns an interface with the values of result object.
-func DynamoDBAttributeResponse(v interface{}, result map[string]*dynamodb.AttributeValue) (interface{}, error) {
+// DynamoDBAttributeResponse returns an item with the values of result object.
+func DynamoDBAttributeResponse(v interface{}, result map[string]*dynamodb.AttributeValue) error {
 	// Unmarshal it into actual interface which front-end can understand as a JSON
 	err := dynamodbattribute.UnmarshalMap(result, v)
 	if err != nil {
 		cw.Error(err, &cw.Logs{Code: "DynamoDBUnmarshalMap", Message: "Failed to unmarshal result map to interface"})
-		return nil, err
+		return err
 	}
 
-	return v, err
+	return nil
+}
+
+// DynamoDBAttributeResponse returns a list of item with the values of result object.
+func DynamoDBAttributesResponse(v interface{}, result []map[string]*dynamodb.AttributeValue) error {
+	// Unmarshal it into actual interface which front-end can understand as a JSON
+	err := dynamodbattribute.UnmarshalListOfMaps(result, v)
+	if err != nil {
+		cw.Error(err, &cw.Logs{Code: "DynamoDBUnmarshalListOfMaps", Message: "Failed to unmarshal result map to interface"})
+		return err
+	}
+
+	return nil
 }

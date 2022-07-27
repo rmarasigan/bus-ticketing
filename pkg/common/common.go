e092b8e94177d846fcd200a227580364b8a8cc3c
rmarasigan <russianhielle@gmail.com>
2022-07-27 12:29:47 +0800

    MOD: regex remove ".", "*", "(", ")", "\"

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -6,10 +6,20 @@ import (
 	cw "github.com/rmarasigan/bus-ticketing/pkg/cw/logger"
 )
 
-// RemoveVowel removes vowel letters from the string passed and
-// returns a string without the vowel letters.
+// RemoveVowel removes vowel letters and some characters (., *, (, ), \)
+// from the string passed and returns a string without the vowel letters.
+//
+// Example:
+//  input, err := RemoveVowel("a(bc\de.f)g*hi")
+//  if err != nil {
+// 	log.Print(err)
+//  }
+//  fmt.Println(input)
+//
+// Output:
+//  bcdfgh
 func RemoveVowel(str string) (string, error) {
-	regex, err := regexp.Compile(`[aeyiou]`)
+	regex, err := regexp.Compile(`[aeiouyAEIOUY\.\*\_\(\)\\]`)
 	if err != nil {
 		cw.Error(err, &cw.Logs{Code: "RemoveVowel", Message: "Failed to parse regex expression"})
 

c7411a02bf9ca3c7a80118ea6fcefde15d10d822
rmarasigan <russianhielle@gmail.com>
2022-07-28 17:24:48 +0800

    ADD: RemoveSymbols and whitespaces MOD: RemoveVowel regex

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -6,20 +6,10 @@ import (
 	cw "github.com/rmarasigan/bus-ticketing/pkg/cw/logger"
 )
 
-// RemoveVowel removes vowel letters and some characters (., *, (, ), \)
-// from the string passed and returns a string without the vowel letters.
-//
-// Example:
-//  input, err := RemoveVowel(`a(bc\de.f)g*hi`)
-//  if err != nil {
-// 	log.Print(err)
-//  }
-//  fmt.Println(input)
-//
-// Output:
-//  bcdfgh
+// RemoveVowel removes vowel letters from the string passed
+// and returns a string without the vowel letters.
 func RemoveVowel(str string) (string, error) {
-	regex, err := regexp.Compile(`[aeiouyAEIOUY\.\*\_\(\)\\]`)
+	regex, err := regexp.Compile(`[aeiouyAEIOUY]`)
 	if err != nil {
 		cw.Error(err, &cw.Logs{Code: "RemoveVowel", Message: "Failed to parse regex expression"})
 
@@ -27,4 +17,17 @@ func RemoveVowel(str string) (string, error) {
 	}
 
 	return regex.ReplaceAllString(str, ""), nil
+}
+
+// RemoveSymbols removes symbols from the string passed and
+// returns a string without the symbols and whitespaces.
+func RemoveSymbols(str string) (string, error) {
+	regex, err := regexp.Compile(`[^\w]`)
+	if err != nil {
+		cw.Error(err, &cw.Logs{Code: "RemoveSymbols", Message: "Failed to parse regex expression."})
+		return "", err
+	}
+
+	return regex.ReplaceAllString(str, ""), nil
+
 }

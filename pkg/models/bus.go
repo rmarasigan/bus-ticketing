7533f4951ecab65080d75fa13df4cc22de0f9c37
rmarasigan <russianhielle@gmail.com>
2022-08-16 12:21:10 +0800

    MOD: BusTrip structure ADD: BusTrip SetValues, SetDefaultValues and IsWithinTheDay function

--- pkg/models/bus.go
+++ pkg/models/bus.go
@@ -1,7 +1,9 @@
 package models
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
@@ -219,10 +221,77 @@ func (route *BusRoute) ValidateUpdate(old *BusRoute) {
 	}
 }
 
+// BusTrip defines the current count of the capacity of a bus for the client to know
+// if there are enough seats for them.
 type BusTrip struct {
-	ID          string   `json:"id"`
-	BusRoute    BusRoute `json:"bus_route"`
-	Status      string   `json:"trip_status"`
-	SeatsLeft   int      `json:"trip_seats_left"`
-	DateCreated string   `json:"trip_date_created"`
+	ID          string `json:"id"`           // Unique bus trip ID as the primary key
+	BusUnit     string `json:"bus_unit"`     // The Bus Unit ID as sort key and for the identification of specific bus trip
+	BusRoute    string `json:"bus_route"`    // The Bus Route ID
+	SeatsLeft   int    `json:"seats_left"`   // Current count left of seats
+	DateCreated string `json:"date_created"` // The date it was created as unix epoch time
+}
+
+// SetValues automatically generates the Bus Trip ID as your primary
+// key, set the date it was created as unix epoch time, and set the
+// seats remaining.
+//
+// Function Parameters:
+//  capacity: The number of bus capacity that a bus can accomodate
+//  seat: The requested number of seats to be reserved by the customer
+func (trip *BusTrip) SetValues(capacity, seat int) {
+	trip.SeatsLeft = (capacity - seat)
+	trip.DateCreated = fmt.Sprint(time.Now().Unix())
+	trip.ID = fmt.Sprintf("%s%s", trip.BusUnit[(len(trip.BusUnit)-6):len(trip.BusUnit)], trip.DateCreated[3:9])
+}
+
+// SetDefaultValues automatically sets the old or default value for the
+// ID, DateCreated and SeatsLeft.
+//
+// Function Parameters:
+//  old: The Bus Trip information that is within the day
+//  seat: The requested number of seats to be reserved by the customer
+func (trip *BusTrip) SetDefaultValues(seat int, old *BusTrip) error {
+	trip.ID = old.ID
+	trip.DateCreated = old.DateCreated
+
+	if old.SeatsLeft <= 0 || seat > old.SeatsLeft {
+		return errors.New("there are not enough seats available")
+	}
+
+	if old.SeatsLeft > 0 && seat <= old.SeatsLeft {
+		trip.SeatsLeft = (old.SeatsLeft - seat)
+	}
+
+	return nil
+}
+
+// IsWithinTheDay validate whether the date and time the trip was created is within the
+// current date range.
+func (trip *BusTrip) IsWithinTheDay() (bool, error) {
+	now := time.Now()
+	layout := "3:04 PM"
+
+	startTime, err := time.Parse(layout, "12:00 AM")
+	if err != nil {
+		cw.Error(err, &cw.Logs{Code: "IsWithinTheDay", Message: "Cannot parse string time to time.Time."})
+		return false, err
+	}
+
+	endTime, err := time.Parse(layout, "11:59 PM")
+	if err != nil {
+		cw.Error(err, &cw.Logs{Code: "IsWithinTheDay", Message: "Cannot parse string to time.Time."})
+		return false, err
+	}
+
+	ending := time.Date(now.Year(), now.Month(), now.Day(), endTime.Hour(), endTime.Minute(), 0, 0, now.Location())
+	beginning := time.Date(now.Year(), now.Month(), now.Day(), startTime.Hour(), startTime.Minute(), 0, 0, now.Location())
+
+	created, err := strconv.Atoi(trip.DateCreated)
+	if err != nil {
+		cw.Error(err, &cw.Logs{Code: "StrconvAtoi", Message: "Failed to convert date created string to int."})
+		return false, err
+	}
+
+	dateCreated := time.Unix(int64(created), 0)
+	return (dateCreated.After(beginning) && dateCreated.Before(ending)), nil
 }
